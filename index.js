54d1f04a1d2b3139caa9a2aa4b7d01322e69d117
김태형 <taehyeong224@gmail.com>
2019-08-09 12:36:32 +0900

    schedule

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { RTMClient } = require('@slack/rtm-api');
 const { WebClient } = require('@slack/web-api');
+const schedule = require('node-schedule');
 
 require('dotenv').config();
 
@@ -24,11 +25,13 @@ rtm.on('ready', () => {
     // web.chat.postMessage({ channel: general, text: '봇 준비 완료', icon_emoji: ":hugging_face:" });
 })
 
+const bansaList = ['바보', '멍청이'];
 rtm.on('message', (message) => {
     try {
         console.log(message);
         const { channel, user, text } = message;
-        if (text === '바보') {
+        const filter = bansaList.filter(s => s === text);
+        if (filter.length > 0) {
             web.chat.postMessage({ channel, text: '반사', icon_emoji: ":raised_hand_with_fingers_splayed:" });
         }
     } catch (e) {
@@ -50,3 +53,7 @@ rtm.on("disconnected", (error) => {
 rtm.on("channel_created", (event) => {
     web.chat.postMessage({ channel: general, text: `#${event.channel.name} 채널이 생성되었습니다.`, icon_emoji: ":raised_hand_with_fingers_splayed:" });
 })
+
+schedule.scheduleJob('0 0 13 * * ?', function () {
+    web.chat.postMessage({ channel: general, text: `점심 맛있게 드셨나요?`, icon_emoji: ":raised_hand_with_fingers_splayed:" });
+});
