8b35f7b2129f92501f0ffc50427ff0e87508597a
김태형 <taehyeong224@gmail.com>
2019-08-10 09:34:52 +0900

    add config.js

---
+++ src/config.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+export const channels = {
+    test: "CM7QQ4VAT",
+    general: "CKC5WGP8B"
+}
+
+export const token = process.env.TOKEN;
+export const general = channels.test;
+
+export const PM10 = {
+    GOOD: 30,
+    NORMAL: 80,
+    BAD: 150,
+    VERY_BAD: 999
+};
+
+export const PM25 = {
+    GOOD: 15,
+    NORMAL: 35,
+    BAD: 75,
+    VERY_BAD: 999
+};
+
+export const TYPE = {
+    PM10: "PM10",
+    PM25: "PM25"
+};
+
+export const DUST_STATUS = {
+    GOOD: "좋음",
+    NORMAL: "보통",
+    BAD: "나쁨",
+    VERY_BAD: "매우 나쁨"
+};
--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import '@babel/polyfill'
 
-import { RTMClient } from "@slack/rtm-api";
-import { WebClient } from "@slack/web-api";
+import {RTMClient} from "@slack/rtm-api";
+import {WebClient} from "@slack/web-api";
 import * as schedule from "node-schedule";
-import { getStatus, TYPE } from "./dust";
-require('dotenv').config();
+import {getStatus} from "./dust";
+import {general, token, TYPE} from "./config";
 
-const channels = {
-    test: "CM7QQ4VAT",
-    general: "CKC5WGP8B"
-}
-
-// An access token (from your Slack app or custom integration - usually xoxb)
-const token = process.env.TOKEN;
-const general = channels.test;
 // The client is initialized and then started to get an active connection to the platform
 const rtm = new RTMClient(token);
 const web = new WebClient(token);
@@ -33,10 +25,9 @@ const dustList = ['미세먼지', '미먼'];
 rtm.on('message', (message) => {
     try {
         console.log(message);
-        const { channel, user, text } = message;
-        const filter = bansaList.filter(s => s === text);
+        const {channel, user, text} = message;
         if (checkHasKeyword(bansaList, text)) {
-            web.chat.postMessage({ channel, text: '반사', icon_emoji: ":raised_hand_with_fingers_splayed:" });
+            web.chat.postMessage({channel, text: '반사', icon_emoji: ":raised_hand_with_fingers_splayed:"});
         }
         if (checkHasKeyword(dustList, text)) {
             dust();
@@ -51,18 +42,22 @@ rtm.on('message', (message) => {
 // After the connection is open, your app will start receiving other events.
 rtm.on('user_typing', (event) => {
     console.log(event);
-})
+});
 
 rtm.on("disconnected", (error) => {
     console.error("disconnected : ", error)
-})
+});
 
 rtm.on("channel_created", (event) => {
-    web.chat.postMessage({ channel: general, text: `#${event.channel.name} 채널이 생성되었습니다.`, icon_emoji: ":raised_hand_with_fingers_splayed:" });
-})
+    web.chat.postMessage({
+        channel: general,
+        text: `#${event.channel.name} 채널이 생성되었습니다.`,
+        icon_emoji: ":raised_hand_with_fingers_splayed:"
+    });
+});
 
 schedule.scheduleJob('0 0 13 * * ?', () => {
-    web.chat.postMessage({ channel: general, text: `점심 맛있게 드셨나요?`, icon_emoji: ":raised_hand_with_fingers_splayed:" });
+    web.chat.postMessage({channel: general, text: `점심 맛있게 드셨나요?`, icon_emoji: ":raised_hand_with_fingers_splayed:"});
 });
 
 schedule.scheduleJob('30 * * * *', () => {
@@ -72,13 +67,15 @@ schedule.scheduleJob('30 * * * *', () => {
 const dust = async () => {
     const pm10 = await getStatus(TYPE.PM10)
     const pm25 = await getStatus(TYPE.PM25)
-    web.chat.postMessage({ channel: general, text: `
+    web.chat.postMessage({
+        channel: general, text: `
 현재 서울 미세먼지
 pm10: ${pm10.data} ${pm10.status}
-pm2.5: ${pm25.data} ${pm25.status}`, icon_emoji: ":mask:" });
-}
+pm2.5: ${pm25.data} ${pm25.status}`, icon_emoji: ":mask:"
+    });
+};
 
 const checkHasKeyword = (list, target) => {
     const filter = list.filter(s => s === target);
     return filter.length > 0;
-}
+};
